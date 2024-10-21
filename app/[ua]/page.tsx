88fd67a8796a43128e96e4a6ec4d069a4bae4e7e
Gregory Reburn <greburn@ebsco.com>
2024-10-21 14:03:04 -0500

    next codemod updates

--- app/[ua]/page.tsx
+++ app/[ua]/page.tsx
@@ -3,22 +3,21 @@ import ClientPage from "../_components/client-page";
 import { Metadata, ResolvingMetadata } from "next";
 
 type Props = {
-	params: { ua: string };
+	params: Promise<{ ua: string }>;
 };
 
-export async function generateMetadata(
-	{ params }: Props,
-	parent: ResolvingMetadata
-): Promise<Metadata> {
-	const userAgent = userAgentFromString(decodeURIComponent(params.ua));
+export async function generateMetadata(props: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    const params = await props.params;
+    const userAgent = userAgentFromString(decodeURIComponent(params.ua));
 
-	return {
+    return {
 		title: `User Agent Breakdown: ${userAgent.browser.name} ${userAgent.browser.version} on ${userAgent?.os.name} ${userAgent?.os.version} (${userAgent?.engine.name} Engine)`,
 		description: `Get information about ${decodeURIComponent(params.ua)}`,
 	};
 }
 
-export default async function Page({ params }: Props) {
-	const userAgent = userAgentFromString(decodeURIComponent(params.ua));
-	return <ClientPage userAgent={userAgent} />;
+export default async function Page(props: Props) {
+    const params = await props.params;
+    const userAgent = userAgentFromString(decodeURIComponent(params.ua));
+    return <ClientPage userAgent={userAgent} />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import { userAgentFromString } from "next/server";
 import ClientPage from "./_components/client-page";
 
 type Props = {
-	searchParams: { ua: string | string[] | undefined };
+	searchParams: Promise<{ ua: string | string[] | undefined }>;
 };
 
-export default async function Page({ searchParams }: Props) {
-	const headers = getHeaders();
-	const uaHeader = headers.get("user-agent");
-	const uaQSP = searchParams.ua;
-	const userAgent = userAgentFromString(
+export default async function Page(props: Props) {
+    const searchParams = await props.searchParams;
+    const headers = await getHeaders();
+    const uaHeader = headers.get("user-agent");
+    const uaQSP = searchParams.ua;
+    const userAgent = userAgentFromString(
 		Array.isArray(uaQSP) ? uaQSP[0] : uaQSP || uaHeader || ""
 	);
-	return <ClientPage userAgent={userAgent} />;
+    return <ClientPage userAgent={userAgent} />;
 }
